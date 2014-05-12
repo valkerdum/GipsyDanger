cc2ac656191ed9d6e30387b8ab32fc2b685f811d
Oscar Fuentes <valkerdum@gmail.com>
2014-05-12 02:47:39 -0400

    mobile

--- js/scripts.js
+++ js/scripts.js
@@ -116,3 +116,15 @@ $(document).ready(function() {
         }
     });
 });
+
+
+
+
+// BOTON GO TOP
+
+  $("a[href='#top']").click(function() {
+     $("html, body").animate({ scrollTop: 0 }, "slow");
+     return false;
+  });
+
+
